7065b6b193e65d8d444cce86bb7dfeb101bd78f5
Craig Drummond <craig.p.drummond@gmail.com>
2020-06-18 14:45:21 +0100

    tabs -> spaces

--- MIPMixer/Plugin.pm
+++ MIPMixer/Plugin.pm
@@ -26,7 +26,7 @@ use Slim::Utils::Strings qw(cstring);
 use Slim::Utils::Prefs;
 
 if ( main::WEBUI ) {
-	require Plugins::MIPMixer::Settings;
+    require Plugins::MIPMixer::Settings;
 }
 
 use Plugins::MIPMixer::Common;
@@ -40,221 +40,221 @@ my $NUM_TRACKS_REPEAT_ARTIST = 5;
 my $NUM_TRACKS_TO_USE = 5;
 
 my $log = Slim::Utils::Log->addLogCategory({
-	'category'     => 'plugin.mipmixer',
-	'defaultLevel' => 'ERROR',
-	'logGroups'    => 'SCANNER',
+    'category'     => 'plugin.mipmixer',
+    'defaultLevel' => 'ERROR',
+    'logGroups'    => 'SCANNER',
 });
 
 my $prefs = preferences('plugin.mipmixer');
 
 sub shutdownPlugin {
-	$initialized = 0;
+    $initialized = 0;
 }
 
 sub initPlugin {
-	my $class = shift;
+    my $class = shift;
 
-	return 1 if $initialized;
+    return 1 if $initialized;
 
-	$prefs->init({
-		mix_type        => 0,
-		mix_style       => 20,
-		mix_variety     => 0,
-		filter_genres   => 0,
-		port            => 10002,
-	});
+    $prefs->init({
+        mix_type        => 0,
+        mix_style       => 20,
+        mix_variety     => 0,
+        filter_genres   => 0,
+        port            => 10002,
+    });
 
-	if ( main::WEBUI ) {
-		Plugins::MIPMixer::Settings->new;
-	}
+    if ( main::WEBUI ) {
+        Plugins::MIPMixer::Settings->new;
+    }
 
-	my $response = _syncHTTPRequest("/api/version");
+    my $response = _syncHTTPRequest("/api/version");
 
-	main::INFOLOG && $log->info("Testing for API on localhost:$MIPPort");
+    main::INFOLOG && $log->info("Testing for API on localhost:$MIPPort");
 
-	if ($response->is_error) {
-		$initialized = 0;
-		$log->error("Can't connect to port $MIPPort - MusicIP disabled.");
+    if ($response->is_error) {
+        $initialized = 0;
+        $log->error("Can't connect to port $MIPPort - MusicIP disabled.");
 
-	} else {
-		my $content = $response->content;
+    } else {
+        my $content = $response->content;
 
-		if ( main::INFOLOG && $log->is_info ) {
-			$log->info($content);
-		}
+        if ( main::INFOLOG && $log->is_info ) {
+            $log->info($content);
+        }
         _initGenres();
-		$initialized = $content;
-	}
-	return $initialized;
+        $initialized = $content;
+    }
+    return $initialized;
 }
 
 sub postinitPlugin {
-	my $class = shift;
+    my $class = shift;
 
-	# if user has the Don't Stop The Music plugin enabled, register ourselves
-	if ( Slim::Utils::PluginManager->isEnabled('Slim::Plugin::DontStopTheMusic::Plugin') ) {
-		require Slim::Plugin::DontStopTheMusic::Plugin;
-		Slim::Plugin::DontStopTheMusic::Plugin->registerHandler('MIPMIXER_MIX', sub {
-			my ($client, $cb) = @_;
+    # if user has the Don't Stop The Music plugin enabled, register ourselves
+    if ( Slim::Utils::PluginManager->isEnabled('Slim::Plugin::DontStopTheMusic::Plugin') ) {
+        require Slim::Plugin::DontStopTheMusic::Plugin;
+        Slim::Plugin::DontStopTheMusic::Plugin->registerHandler('MIPMIXER_MIX', sub {
+            my ($client, $cb) = @_;
 
-			my $seedTracks = Slim::Plugin::DontStopTheMusic::Plugin->getMixableProperties($client, 5);
-			my $tracks = [];
+            my $seedTracks = Slim::Plugin::DontStopTheMusic::Plugin->getMixableProperties($client, 5);
+            my $tracks = [];
 
-			# don't seed from radio stations - only do if we're playing from some track based source
-			if ($seedTracks && ref $seedTracks && scalar @$seedTracks) {
-				foreach my $seedTrack (@$seedTracks) {
-					my ($trackObj) = Slim::Schema->find('Track', $seedTrack->{id});
-					if ($trackObj) {
-					    my $mix = _getMix($trackObj);
-					    main::idleStreams();
+            # don't seed from radio stations - only do if we're playing from some track based source
+            if ($seedTracks && ref $seedTracks && scalar @$seedTracks) {
+                foreach my $seedTrack (@$seedTracks) {
+                    my ($trackObj) = Slim::Schema->find('Track', $seedTrack->{id});
+                    if ($trackObj) {
+                        my $mix = _getMix($trackObj);
+                        main::idleStreams();
 
-					    if ($mix && scalar @$mix) {
-					  	    my @genres = _getSeedGenres($trackObj->id);
-					  	    if (scalar @genres > 1) {
-					  	        # Track's genre is listed in genres.json, so only allow similar genres
-					            my %genrehash = map { $_ => 1 } @genres;
-					            foreach my $candidate (@$mix) {
-					                my @cgenres = _getCandidateGenres($candidate);
-                                	my $count = scalar @cgenres;
-                                	my $found = 0;
+                        if ($mix && scalar @$mix) {
+                              my @genres = _getSeedGenres($trackObj->id);
+                              if (scalar @genres > 1) {
+                                  # Track's genre is listed in genres.json, so only allow similar genres
+                                my %genrehash = map { $_ => 1 } @genres;
+                                foreach my $candidate (@$mix) {
+                                    my @cgenres = _getCandidateGenres($candidate);
+                                    my $count = scalar @cgenres;
+                                    my $found = 0;
 
-                                	for (my $i = 0; $i < $count && $found==0; $i++) {
-					                    if (exists($genrehash{$cgenres[$i]})) {
-					                        main::DEBUGLOG && $log->debug($candidate . " matched genre");
-        						            push @$tracks, $candidate;
-        						            $found=1;
-        						        }
-        						    }
-        						    
-        						    main::DEBUGLOG && $found==0 && $log->debug($candidate . " FAILED to match genre");
-        						}
-    						} else {
-    						    # Track's genre is not in genres.json - so can't filter!
-    						    push @$tracks, @$mix;
-    						}
-					    }
-					}
-				}
-			}
+                                    for (my $i = 0; $i < $count && $found==0; $i++) {
+                                        if (exists($genrehash{$cgenres[$i]})) {
+                                            main::DEBUGLOG && $log->debug($candidate . " matched genre");
+                                            push @$tracks, $candidate;
+                                            $found=1;
+                                        }
+                                    }
 
-			$tracks = Slim::Plugin::DontStopTheMusic::Plugin->deDupe($tracks);
-			if ( scalar @$tracks > $NUM_TRACKS_TO_USE ) {
-				Slim::Player::Playlist::fischer_yates_shuffle($tracks);
-				$tracks = [ splice(@$tracks, 0, $NUM_TRACKS_TO_USE) ];
-			}
+                                    main::DEBUGLOG && $found==0 && $log->debug($candidate . " FAILED to match genre");
+                                }
+                            } else {
+                                # Track's genre is not in genres.json - so can't filter!
+                                push @$tracks, @$mix;
+                            }
+                        }
+                    }
+                }
+            }
 
-			$cb->($client, $tracks);
-		});
-	}
+            $tracks = Slim::Plugin::DontStopTheMusic::Plugin->deDupe($tracks);
+            if ( scalar @$tracks > $NUM_TRACKS_TO_USE ) {
+                Slim::Player::Playlist::fischer_yates_shuffle($tracks);
+                $tracks = [ splice(@$tracks, 0, $NUM_TRACKS_TO_USE) ];
+            }
+
+            $cb->($client, $tracks);
+        });
+    }
 }
 
 sub prefName {
-	my $class = shift;
-	return lc($class->title);
+    my $class = shift;
+    return lc($class->title);
 }
 
 sub title {
-	my $class = shift;
-	return 'MIPMIXER';
+    my $class = shift;
+    return 'MIPMIXER';
 }
 
 sub _getMix {
-	my $track = shift;
+    my $track = shift;
 
-	my $id = index($track->url, '#')>0 ? $track->url : $track->path;
-	my @mix = ();
-	my $req;
-	my $res;
+    my $id = index($track->url, '#')>0 ? $track->url : $track->path;
+    my @mix = ();
+    my $req;
+    my $res;
 
-	my %args = (
-			# Set the size of the list
-			'size'       => $NUM_TRACKS,
-			'sizetype'   => 'tracks',
+    my %args = (
+            # Set the size of the list
+            'size'       => $NUM_TRACKS,
+            'sizetype'   => 'tracks',
 
-			# Set the style slider (default 20)
-			'style'      => $prefs->get('mix_style'),
+            # Set the style slider (default 20)
+            'style'      => $prefs->get('mix_style'),
 
-			# Set the variety slider (default 0)
-			'variety'    => $prefs->get('mix_variety'),
+            # Set the variety slider (default 0)
+            'variety'    => $prefs->get('mix_variety'),
 
             # Don't restrict to genre of current track - we will (optionally) filter results
-			'mixgenre'   => '0',
+            'mixgenre'   => '0',
 
-			# Set the number of songs before allowing dupes
-			'rejectsize' => $NUM_TRACKS_REPEAT_ARTIST,
-			'rejecttype' => 'tracks'
-		);
+            # Set the number of songs before allowing dupes
+            'rejectsize' => $NUM_TRACKS_REPEAT_ARTIST,
+            'rejecttype' => 'tracks'
+        );
 
-	my $filter = $prefs->get('mix_filter');
+    my $filter = $prefs->get('mix_filter');
 
-	if ($filter) {
-		$filter = Slim::Utils::Unicode::utf8decode_locale($filter);
+    if ($filter) {
+        $filter = Slim::Utils::Unicode::utf8decode_locale($filter);
 
-		main::DEBUGLOG && $log->debug("Filter $filter in use.");
+        main::DEBUGLOG && $log->debug("Filter $filter in use.");
 
-		$args{'filter'} = Plugins::MIPMixer::Common::escape($filter);
-	}
+        $args{'filter'} = Plugins::MIPMixer::Common::escape($filter);
+    }
 
-	my $argString = join( '&', map { "$_=$args{$_}" } keys %args );
+    my $argString = join( '&', map { "$_=$args{$_}" } keys %args );
 
-	main::DEBUGLOG && $log->debug("Creating mix using: $id as seed.");
+    main::DEBUGLOG && $log->debug("Creating mix using: $id as seed.");
 
-	if (!main::ISWINDOWS) {
-		# need to decode the file path when a file is used as seed
-		$id = Slim::Utils::Unicode::utf8decode_locale($id);
-	}
+    if (!main::ISWINDOWS) {
+        # need to decode the file path when a file is used as seed
+        $id = Slim::Utils::Unicode::utf8decode_locale($id);
+    }
 
-	# url encode the request, but not the argstring
-	my $mixArgs = 'song=' . Plugins::MIPMixer::Common::escape($id);
+    # url encode the request, but not the argstring
+    my $mixArgs = 'song=' . Plugins::MIPMixer::Common::escape($id);
 
-	main::DEBUGLOG && $log->debug("Request http://localhost:$MIPPort/api/mix?$mixArgs\&$argString");
+    main::DEBUGLOG && $log->debug("Request http://localhost:$MIPPort/api/mix?$mixArgs\&$argString");
 
-	my $response = _syncHTTPRequest("/api/mix?$mixArgs\&$argString");
+    my $response = _syncHTTPRequest("/api/mix?$mixArgs\&$argString");
 
-	if ($response->is_error) {
-		if ($response->code == 500 && $filter) {
-			::idleStreams();
+    if ($response->is_error) {
+        if ($response->code == 500 && $filter) {
+            ::idleStreams();
 
-			# try again without the filter
-			$log->warn("No mix returned with filter involved - we might want to try without it");
-			$argString =~ s/filter=/xfilter=/;
-			$response = _syncHTTPRequest("/api/mix?$mixArgs\&$argString");
+            # try again without the filter
+            $log->warn("No mix returned with filter involved - we might want to try without it");
+            $argString =~ s/filter=/xfilter=/;
+            $response = _syncHTTPRequest("/api/mix?$mixArgs\&$argString");
 
-			Plugins::MIPMixer::Common->grabFilters();
-		}
+            Plugins::MIPMixer::Common->grabFilters();
+        }
 
-		if ($response->is_error) {
-			$log->warn("Warning: Couldn't get mix: $mixArgs\&$argString");
-			main::DEBUGLOG && $log->debug($response->as_string);
-			return \@mix;
-		}
-	}
+        if ($response->is_error) {
+            $log->warn("Warning: Couldn't get mix: $mixArgs\&$argString");
+            main::DEBUGLOG && $log->debug($response->as_string);
+            return \@mix;
+        }
+    }
 
-	my @songs = split(/\n/, $response->content);
-	my $count = scalar @songs;
+    my @songs = split(/\n/, $response->content);
+    my $count = scalar @songs;
 
-	for (my $j = 0; $j < $count; $j++) {
-		# Bug 4281 - need to convert from UTF-8 on Windows.
-		if (main::ISWINDOWS && !-e $songs[$j] && -e Win32::GetANSIPathName($songs[$j])) {
-			$songs[$j] = Win32::GetANSIPathName($songs[$j]);
-		}
+    for (my $j = 0; $j < $count; $j++) {
+        # Bug 4281 - need to convert from UTF-8 on Windows.
+        if (main::ISWINDOWS && !-e $songs[$j] && -e Win32::GetANSIPathName($songs[$j])) {
+            $songs[$j] = Win32::GetANSIPathName($songs[$j]);
+        }
 
-		if ( -e $songs[$j] || -e Slim::Utils::Unicode::utf8encode_locale($songs[$j]) || index($songs[$j], 'file:///')==0) {
-			push @mix, Slim::Utils::Misc::fileURLFromPath($songs[$j]);
-		} else {
-			$log->error('MIP attempted to mix in a song at ' . $songs[$j] . ' that can\'t be found at that location');
-		}
-	}
+        if ( -e $songs[$j] || -e Slim::Utils::Unicode::utf8encode_locale($songs[$j]) || index($songs[$j], 'file:///')==0) {
+            push @mix, Slim::Utils::Misc::fileURLFromPath($songs[$j]);
+        } else {
+            $log->error('MIP attempted to mix in a song at ' . $songs[$j] . ' that can\'t be found at that location');
+        }
+    }
 
-	return \@mix;
+    return \@mix;
 }
 
 sub _syncHTTPRequest {
-	my $url = shift;
-	$MIPPort = $prefs->get('port') unless $MIPPort;
-	my $http = LWP::UserAgent->new;
-	$http->timeout($prefs->get('timeout') || 5);
-	return $http->get("http://localhost:$MIPPort$url");
+    my $url = shift;
+    $MIPPort = $prefs->get('port') unless $MIPPort;
+    my $http = LWP::UserAgent->new;
+    $http->timeout($prefs->get('timeout') || 5);
+    return $http->get("http://localhost:$MIPPort$url");
 }
 
 sub _initGenres {
