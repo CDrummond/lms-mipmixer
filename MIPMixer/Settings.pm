fe9a8bec61235cda172cc9c4013f920503d44844
Craig Drummond <craig.p.drummond@gmail.com>
2022-01-08 21:27:28 +0000

    Update (c) year

--- MIPMixer/Settings.pm
+++ MIPMixer/Settings.pm
@@ -3,7 +3,7 @@ package Plugins::MIPMixer::Settings;
 #
 # LMS-MIPMixer
 #
-# Copyright (c) 2020-2021 Craig Drummond <craig.p.drummond@gmail.com>
+# Copyright (c) 2020-2022 Craig Drummond <craig.p.drummond@gmail.com>
 #
 # GPLv2 license.
 #
